7e97d2d53f90d93d88f779bd63d4a87422712c90
returnanapple <237680317@qq.com>
2013-10-08 16:12:53 +0800

    second

---
+++ ToClient/Controls/FriendListTittleButton.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ToClient.Controls
+{
+    public partial class FriendListTittleButton : UserControl
+    {
+        public FriendListTittleButton()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(FriendListTittleButton), new PropertyMetadata(false));
+
+
+
+        public string TittleText
+        {
+            get { return (string)GetValue(TittleTextProperty); }
+            set { SetValue(TittleTextProperty, value); }
+        }
+        public static readonly DependencyProperty TittleTextProperty =
+            DependencyProperty.Register("TittleText", typeof(string), typeof(FriendListTittleButton), new PropertyMetadata(""));
+
+
+
+        public string OnLineFriendsCount
+        {
+            get { return (string)GetValue(OnLineFriendsCountProperty); }
+            set { SetValue(OnLineFriendsCountProperty, value); }
+        }
+        public static readonly DependencyProperty OnLineFriendsCountProperty =
+            DependencyProperty.Register("OnLineFriendsCount", typeof(string), typeof(FriendListTittleButton), new PropertyMetadata(""));
+
+        public string AllFriendsCount
+        {
+            get { return (string)GetValue(AllFriendsCountProperty); }
+            set { SetValue(AllFriendsCountProperty, value); }
+        }
+        public static readonly DependencyProperty AllFriendsCountProperty =
+            DependencyProperty.Register("AllFriendsCount", typeof(string), typeof(FriendListTittleButton), new PropertyMetadata(""));
+
+        public void MouseEnterAction(object sender, MouseEventArgs e)
+        {
+            (sender as Grid).Background = new SolidColorBrush(Color.FromArgb(255, 229, 229, 227));
+        }
+
+        public void MouseLeaveAction(object sender, MouseEventArgs e)
+        {
+            (sender as Grid).Background = new SolidColorBrush(Colors.White);
+            
+        }
+
+        public void MouseLeftButtonDownAction(object sender, MouseButtonEventArgs e)
+        {
+            IsOpen = IsOpen == false ? true : false;
+        }
+    }
+}
---
+++ ToClient/ToClientVM.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ToClient
+{
+    public class ToClientVM:INotifyPropertyChanged
+    {
+        #region 私有字段
+        private bool chatWindowIsOpen;
+        private bool friendListWindowIsOpen;
+        private int newMessageCount;
+
+        private bool customerServiceListIsOpen;
+        private bool superiorListIsOpen;
+        private bool lowerListIsOpen;
+        #endregion
+        #region 属性
+        public bool CustomerServiceListIsOpen
+        {
+            get
+            { return customerServiceListIsOpen; }
+            set
+            {
+                customerServiceListIsOpen = value;
+                OnPropertyChanged(this, "CustomerServiceListIsOpen");
+            }
+        }
+        public bool SuperiorListIsOpen
+        {
+            get
+            { return superiorListIsOpen; }
+            set
+            {
+                superiorListIsOpen = value;
+                OnPropertyChanged(this, "SuperiorListIsOpen");
+            }
+        }
+        public bool LowerListIsOpen
+        {
+            get
+            { return lowerListIsOpen; }
+            set 
+            {
+                lowerListIsOpen = value;
+                OnPropertyChanged(this, "LowerListIsOpen");
+            }
+        }
+        #endregion
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(object sender, string property)
+        {
+            PropertyChanged(sender, new PropertyChangedEventArgs(property));
+        }
+    }
+}
